d2493122645398b682930b7b2377689b849b8ee3
Shervin Jafari <shv.jafari@gmail.com>
2017-01-30 13:51:57 +0100

    checkpoint-11-DOM-album-view

--- scripts/album.js
+++ scripts/album.js
@@ -3,7 +3,7 @@ var albumPicasso = {
   artist: 'Pablo Picasso',
   label: 'Cubism',
   year: '1881',
-  albumArtUrl: 'assets/images/album_cover/01.png',
+  albumArtUrl: 'assets/images/album_covers/01.png',
   songs: [
     { title: 'Blue', duration: '4:26' },
     { title: 'Green', duration: '3:14' },
@@ -43,14 +43,14 @@ var createSongRow = function(songNumber, songName, songLength) {
 var setCurrentAlbum = function(album) {
   var albumTitle = document.getElementsByClassName('album-view-title')[0];
   var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-  var albumReleaseInfo = document.getElementsByClassName('album-release-info')[0];
+  var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
   var albumImage = document.getElementsByClassName('album-cover-art')[0];
   var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
   albumTitle.firstChild.nodeValue = album.title;
   albumArtist.firstChild.nodeValue = album.artist;
   albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-  albumImage.setAttribute('src, album.albumArtUrl');
+  albumImage.setAttribute('src', album.albumArtUrl);
   
   albumSongList.innerHTML = '';
   
