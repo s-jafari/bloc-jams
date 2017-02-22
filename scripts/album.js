07fc843e26c6661465a2ef19304078c2480d09ff
Shervin Jafari <shv.jafari@gmail.com>
2017-02-22 19:15:39 +0100

    player bar play pause functionality

--- scripts/album.js
+++ scripts/album.js
@@ -244,6 +244,18 @@ var previousSong = function() {
     
 };
 
+var togglePlayFromPlayerBar = function() {
+  if(currentSoundFile.isPaused()){
+    getSongNumberCell(currentlyPlayingSongNumber).html(pauseButtonTemplate);
+    $playPauseButton.html(playerBarPauseButton);
+    currentSoundFile.play();
+  } else if(currentSoundFile) {
+    getSongNumberCell(currentlyPlayingSongNumber).html(playButtonTemplate);
+    $playPauseButton.html(playerBarPlayButton);
+    currentSoundFile.pause();
+  }
+};
+
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 var playerBarPlayButton = '<span class="ion-play"></span>';
@@ -257,10 +269,12 @@ var currentVolume = 80;
 
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
+var $playPauseButton = $('.main-controls .play-pause');
 
 $(document).ready(function() {
   setCurrentAlbum(albumPicasso);
   setupSeekBars();
   $previousButton.click(previousSong);
   $nextButton.click(nextSong);
+  $playPauseButton.click(togglePlayFromPlayerBar);
 });
